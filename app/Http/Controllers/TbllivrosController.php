4af5c9fae5f82d056b36dffdb9395e53ca08c306
liliamsilva1 <gustavo.correiademoraes2006@gmail.com>
2024-08-02 22:55:45 -0300

    funçõe croud

---
+++ app/Http/Controllers/TbllivrosController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TbllivrosController extends Controller
+{
+    //construir o crud
+    //mostrar todos os registros da tabeka livros
+    //crud -> Read(leitura) select/ visualizar
+    public function index() {
+
+    }
+    //cadastrar registro
+    //Crud -> Create(criar/cadastrar)
+    public function store (){
+
+    }
+    //Alterar registro
+    //Crud -> update(alterar)
+    public function update (){
+
+    }
+    //Deletar os registros
+    //Crud -> delete(apagar)
+    public function destroy (){
+
+    }
+}
